c91443e2b5935981fed6d04a7a2bdbc8fa7a1247
ABE93 <fabian.abele@live.de>
2017-06-01 16:14:04 +0200

    Added get_nickname wich extrakts the nickname from the JWT. Modifed the auth method to work with JWT.

--- auth.py
+++ auth.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 
-import sys, time
+import sys, time, jwt
 from struct import *
+
+from pymongo import MongoClient
+
+SECRET_KEY = "geheimnisDesGrauens"
+mongoUrl = "mongodb://localhost:27017/friendscomm"
 
 def log(string):
     a = open('auth.log', 'a')
@@ -23,14 +28,39 @@ def to_ejabberd(bool):
     sys.stdout.flush()
 
 def auth(username, server, password):
-    log('doing auth:' + username + ':' + server + ':' + password)
-    return True
+	auth_token = password
+	nickname = get_nickname(auth_token)
+	log('doing auth for:' + nickame)
+	client = MongoClient(mongoUrl)
+	collection = client.users
+	result = str(client.user.find_one({"Nickname":nickname}))
+	comp = str('None')
+	if result == comp:
+		log('access granted')
+		return False
+	else:
+		log('access denied invalid user')
+		return True 
 
 def isuser(username, server):
     return True
 
 def setpass(username, server, password):
     return True
+
+
+
+def get_nickname(auth_token):
+    try:
+        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+        return payload['nickname']
+    except jwt.ExpiredSignatureError:
+		log('Signature expired.')
+        return False
+    except jwt.InvalidTokenError:
+		log('Invalid token.')
+        return False
+
 
 	
 while True:
